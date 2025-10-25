6d93572328d5b75444c5bd17307233b31321c8e8
Hylra-2105 <loilt.ce190481@gmail.com>
2025-10-25 09:55:00 +0700

    Update validate v2

--- assets/js/account.js
+++ assets/js/account.js
@@ -133,3 +133,99 @@ if (registerForm) {
     });
 }
 // End Register Form
+
+// Forgot Password Form
+const forgotPasswordForm = document.querySelector("#forgot-password-form");
+if (forgotPasswordForm) {
+  const validation = new JustValidate("#forgot-password-form");
+
+  validation
+    .addField("#email", [
+      {
+        rule: "required",
+        errorMessage: "Vui lòng nhập email của bạn!",
+      },
+      {
+        rule: "email",
+        errorMessage: "Email không đúng định dạng!",
+      },
+    ])
+    .onSuccess((event) => {
+      const email = event.target.email.value;
+      console.log(email);
+    });
+}
+// End Forgot Password Form
+
+// OTP Password Form
+const otpPasswordForm = document.querySelector("#otp-password-form");
+if (otpPasswordForm) {
+  const validation = new JustValidate("#otp-password-form");
+
+  validation
+    .addField("#otp", [
+      {
+        rule: "required",
+        errorMessage: "Vui lòng nhập mã OTPS!",
+      },
+    ])
+    .onSuccess((event) => {
+      const otp = event.target.otp.value;
+      console.log(otp);
+    });
+}
+// End OTP Password Form
+
+// Reset Password Form
+const resetPasswordForm = document.querySelector("#reset-password-form");
+if (resetPasswordForm) {
+  const validator = new JustValidate("#reset-password-form");
+
+  validator
+    .addField("#password", [
+      {
+        rule: "required",
+        errorMessage: "Vui lòng nhập mật khẩu!",
+      },
+      {
+        rule: "minLength",
+        value: 8,
+        errorMessage: "Mật khẩu phải chứa ít nhất 8 ký tự!",
+      },
+      {
+        rule: "customRegexp",
+        value: /[A-Z]/,
+        errorMessage: "Mật khẩu phải chứa ít nhất 1 ký tự viết hoa!",
+      },
+      {
+        rule: "customRegexp",
+        value: /[a-z]/,
+        errorMessage: "Mật khẩu phải chứa ít nhất 1 ký tự viết thường!",
+      },
+      {
+        rule: "customRegexp",
+        value: /[0-9]/,
+        errorMessage: "Mật khẩu phải chứa ít nhất 1 chữ số!",
+      },
+      {
+        rule: "customRegexp",
+        value: /^(?=.*[!@#$%^&*(),.?":{}|<>_\-]).+$/,
+        errorMessage: "Mật khẩu phải chứa ít nhất 1 ký tự đặc biệt!",
+      },
+    ])
+    .addField("#confirmPassword", [
+      {
+        rule: "required",
+        errorMessage: "Vui lòng nhập xác nhậnmật khẩu!",
+      },
+      {
+        validator: (value, context) => context["#password"].elem.value == value,
+        errorMessage: "Mật khẩu xác nhận không khớp!",
+      },
+    ])
+    .onSuccess((event) => {
+      const password = event.target.password.value;
+      console.log(password);
+    });
+}
+// End Reset Password Form
--- assets/js/script.js
+++ assets/js/script.js
@@ -84,18 +84,68 @@ if (filepondImage) {
 // Revenue Chart
 const revenueChart = document.querySelector("#revenue-chart");
 if (revenueChart) {
+  // lấy ngày hiện tại
+  const now = new Date();
+
+  // lấy ra thông tin tháng này
+  const currentMonth = now.getMonth() + 1;
+  const currentYear = now.getFullYear();
+
+  // lấy ra thông tin tháng trước
+  const previousMonthDate = new Date(currentYear, now.getMonth() - 1, 1);
+  const previousMonth = previousMonthDate.getMonth() + 1;
+  const previousYear = previousMonthDate.getFullYear();
+
+  // lấy ra tổng số ngày
+  const datsInMonthCurrent = new Date(currentYear, currentMonth, 0).getDate();
+  const datsInMonthPrevious = new Date(
+    previousYear,
+    previousMonth,
+    0
+  ).getDate();
+  const days =
+    datsInMonthCurrent > datsInMonthPrevious
+      ? datsInMonthCurrent
+      : datsInMonthPrevious;
+  const arrayDay = [];
+  for (let i = 1; i <= days; i++) {
+    arrayDay.push(i);
+  }
+
+  const dataFInal = {
+    currentMonth: currentMonth,
+    currentYear: currentYear,
+    previousMonth: previousMonth,
+    previousYear: previousYear,
+    arrayDay: arrayDay,
+  };
+
+  // fetch(`/${pathAdmin}/dashboard/revenue-chart`, {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(dataFinal),
+  // })
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     console.log(data);
+  //   });
+
   new Chart(revenueChart, {
     type: "line",
     data: {
-      labels: ["01", "02", "03", "04", "05"],
+      labels: arrayDay,
       datasets: [
+        // tháng hiện tại
         {
-          label: "Tháng 09/2025",
+          label: `Tháng ${currentMonth}/${currentYear}`,
           data: [10000, 500, 3000, 200, 1500],
           borderColor: "#F38FA4",
         },
+        // tháng trước
         {
-          label: "Tháng 10/2025",
+          label: `Tháng ${previousMonth}/${previousYear}`,
           data: [15000, 5000, 300, 20000, 15000],
           borderColor: "#26A2F5",
         },
@@ -135,3 +185,274 @@ if (revenueChart) {
   });
 }
 // End Revenue Chart
+
+// Category Create Form
+const categoryCreateForm = document.querySelector("#category-create-form");
+if (categoryCreateForm) {
+  const validation = new JustValidate("#category-create-form");
+
+  validation
+    .addField("#name", [
+      {
+        rule: "required",
+        errorMessage: "Vui lòng nhập tên danh mục",
+      },
+    ])
+    .onSuccess((event) => {
+      const name = event.target.name.value;
+      console.log(name);
+    });
+}
+// End Category Create Form
+
+// Tour Create Form
+const tourCreateForm = document.querySelector("#tour-create-form");
+if (tourCreateForm) {
+  const validation = new JustValidate("#tour-create-form");
+
+  validation
+    .addField("#name", [
+      {
+        rule: "required",
+        errorMessage: "Vui lòng nhập tên danh mục",
+      },
+    ])
+    .onSuccess((event) => {
+      const name = event.target.name.value;
+      console.log(name);
+    });
+}
+// End Tour Create Form
+
+// Order Edit Form
+const orderEditForm = document.querySelector("#order-edit-form");
+if (orderEditForm) {
+  const validation = new JustValidate("#order-edit-form");
+
+  validation
+    .addField("#fullName", [
+      {
+        rule: "required",
+        errorMessage: "Vui lòng nhập họ tên!",
+      },
+      {
+        rule: "minLength",
+        value: 5,
+        errorMessage: "Họ tên phải có ít nhất 5 ký tự!",
+      },
+      {
+        rule: "maxLength",
+        value: 50,
+        errorMessage: "Họ tên không được vượt quá 50 ký tự!",
+      },
+    ])
+    .addField("#phone", [
+      {
+        rule: "required",
+        errorMessage: "Vui lòng nhập số điện thoại!",
+      },
+      {
+        rule: "customRegexp",
+        value: /^(0|\+84)(3[2-9]|5[6|8|9]|7[0|6-9]|8[1-5]|9[0-9])[0-9]{7}$/,
+        errorMessage: "Số điện thoại không đúng định dạng!",
+      },
+    ])
+    .onSuccess((event) => {
+      const fullName = event.target.fullName.value;
+      console.log(fullName);
+      const phone = event.target.phone.value;
+      console.log(phone);
+    });
+}
+// End Order Edit Form
+
+// Setting Website Info Form
+const settingWebsiteInfoForm = document.querySelector(
+  "#setting-website-infor-form"
+);
+if (settingWebsiteInfoForm) {
+  const validation = new JustValidate("#setting-website-infor-form");
+
+  validation
+    .addField("#websiteName", [
+      {
+        rule: "required",
+        errorMessage: "Vui lòng nhập tên website!",
+      },
+    ])
+    .addField("#phone", [
+      {
+        rule: "customRegexp",
+        value: /^(0|\+84)(3[2-9]|5[6|8|9]|7[0|6-9]|8[1-5]|9[0-9])[0-9]{7}$/,
+        errorMessage: "Số điện thoại không đúng định dạng!",
+      },
+    ])
+    .addField("#email", [
+      {
+        rule: "email",
+        errorMessage: "Email không đúng định dạng!",
+      },
+    ])
+    .onSuccess((event) => {
+      const websiteName = event.target.websiteName.value;
+      const phone = event.target.phone.value;
+      const email = event.target.email.value;
+      console.log(websiteName);
+      console.log(phone);
+      console.log(email);
+    });
+}
+// End Setting Website Info Form
+
+// Setting Account Admin Create Form
+const settingAccountAdminCreateForm = document.querySelector(
+  "#setting-account-admin-create-form"
+);
+if (settingAccountAdminCreateForm) {
+  const validation = new JustValidate("#setting-account-admin-create-form");
+
+  validation
+    .addField("#fullName", [
+      {
+        rule: "required",
+        errorMessage: "Vui lòng nhập họ tên!",
+      },
+      {
+        rule: "minLength",
+        value: 5,
+        errorMessage: "Họ tên phải có ít nhất 5 ký tự!",
+      },
+      {
+        rule: "maxLength",
+        value: 50,
+        errorMessage: "Họ tên không được vượt quá 50 ký tự!",
+      },
+    ])
+    .addField("#email", [
+      {
+        rule: "required",
+        errorMessage: "Vui lòng nhập tên email!",
+      },
+      {
+        rule: "email",
+        errorMessage: "Email không đúng định dạng!",
+      },
+    ])
+    .addField("#phone", [
+      {
+        rule: "required",
+        errorMessage: "Vui lòng nhập tên số điện thoại!",
+      },
+      {
+        rule: "customRegexp",
+        value: /^(0|\+84)(3[2-9]|5[6|8|9]|7[0|6-9]|8[1-5]|9[0-9])[0-9]{7}$/,
+        errorMessage: "Số điện thoại không đúng định dạng!",
+      },
+    ])
+    .addField("#jobTitle", [
+      {
+        rule: "required",
+        errorMessage: "Vui lòng nhập chức vụ!",
+      },
+    ])
+    .addField("#password", [
+      {
+        rule: "required",
+        errorMessage: "Vui lòng nhập mật khẩu!",
+      },
+      {
+        rule: "minLength",
+        value: 8,
+        errorMessage: "Mật khẩu phải chứa ít nhất 8 ký tự!",
+      },
+      {
+        rule: "customRegexp",
+        value: /[A-Z]/,
+        errorMessage: "Mật khẩu phải chứa ít nhất 1 ký tự viết hoa!",
+      },
+      {
+        rule: "customRegexp",
+        value: /[a-z]/,
+        errorMessage: "Mật khẩu phải chứa ít nhất 1 ký tự viết thường!",
+      },
+      {
+        rule: "customRegexp",
+        value: /[0-9]/,
+        errorMessage: "Mật khẩu phải chứa ít nhất 1 chữ số!",
+      },
+      {
+        rule: "customRegexp",
+        value: /^(?=.*[!@#$%^&*(),.?":{}|<>_\-]).+$/,
+        errorMessage: "Mật khẩu phải chứa ít nhất 1 ký tự đặc biệt!",
+      },
+    ])
+    .onSuccess((event) => {
+      const fullName = event.target.fullName.value;
+      const email = event.target.email.value;
+      const phone = event.target.phone.value;
+      const jobTitle = event.target.jobTitle.value;
+      const password = event.target.password.value;
+      console.log(fullName);
+      console.log(email);
+      console.log(phone);
+      console.log(jobTitle);
+      console.log(password);
+    });
+}
+// End Setting Account Admin Create Form
+
+// Profile Change Password Form
+const profileChangePasswordForm = document.querySelector(
+  "#profile-change-password-form"
+);
+if (profileChangePasswordForm) {
+  const validator = new JustValidate("#profile-change-password-form");
+
+  validator
+    .addField("#password", [
+      {
+        rule: "required",
+        errorMessage: "Vui lòng nhập mật khẩu!",
+      },
+      {
+        rule: "minLength",
+        value: 8,
+        errorMessage: "Mật khẩu phải chứa ít nhất 8 ký tự!",
+      },
+      {
+        rule: "customRegexp",
+        value: /[A-Z]/,
+        errorMessage: "Mật khẩu phải chứa ít nhất 1 ký tự viết hoa!",
+      },
+      {
+        rule: "customRegexp",
+        value: /[a-z]/,
+        errorMessage: "Mật khẩu phải chứa ít nhất 1 ký tự viết thường!",
+      },
+      {
+        rule: "customRegexp",
+        value: /[0-9]/,
+        errorMessage: "Mật khẩu phải chứa ít nhất 1 chữ số!",
+      },
+      {
+        rule: "customRegexp",
+        value: /^(?=.*[!@#$%^&*(),.?":{}|<>_\-]).+$/,
+        errorMessage: "Mật khẩu phải chứa ít nhất 1 ký tự đặc biệt!",
+      },
+    ])
+    .addField("#newPassword", [
+      {
+        rule: "required",
+        errorMessage: "Vui lòng nhập xác nhậnmật khẩu!",
+      },
+      {
+        validator: (value, context) => context["#password"].elem.value == value,
+        errorMessage: "Mật khẩu xác nhận không khớp!",
+      },
+    ])
+    .onSuccess((event) => {
+      const password = event.target.password.value;
+      console.log(password);
+    });
+}
+// End Profile Change Password Form
